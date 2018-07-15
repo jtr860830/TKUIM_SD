e6ecaf0cfd206f67b518feadb82bb31b393d135c
Jezrien Hsieh <jtr860830@yahoo.com.tw>
2018-07-16 01:39:01 +0800

    Complete a simple authentication
    issue: cookie-based session token is not safe enough

--- go.mod
+++ go.mod
@@ -1,13 +1,19 @@
 module github.com/jtr860830/SD-Backend
 
 require (
-	github.com/gin-contrib/sessions v0.0.0-20180702031852-da25b7bed527 // indirect
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/gin-contrib/sessions v0.0.0-20180702031852-da25b7bed527
 	github.com/gin-gonic/gin v1.1.4
 	github.com/golang/protobuf v1.1.0 // indirect
 	github.com/gorilla/sessions v1.1.1 // indirect
 	github.com/manucorporat/sse v0.0.0-20160126180136-ee05b128a739 // indirect
 	github.com/mattn/go-isatty v0.0.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
 	golang.org/x/net v0.0.0-20180712202826-d0887baf81f4 // indirect
+	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
+	golang.org/x/sys v0.0.0-20180715085529-ac767d655b30 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/yaml.v2 v2.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main // import "github.com/jtr860830/SD-Backend"
 
 import (
-	//"log"
+	"log"
 	"net/http"
-	//"strings"
+	"strings"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
@@ -13,7 +13,7 @@ import (
 func main() {
 	route := gin.Default()
 	store := cookie.NewStore([]byte("secret-string"))
-	route.Use(sessions.Sessions("mysession", store))
+	route.Use(sessions.Sessions("session", store))
 
 	route.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Home Page")
@@ -22,17 +22,72 @@ func main() {
 	route.GET("/logout", logoutHandler)
 	route.POST("/register", registerHandler)
 
+	account := route.Group("/user")
+	{
+		account.GET("/", func(c *gin.Context) {
+			c.Redirect(301, "/user/profile")
+		})
+		account.GET("/profile", profileHandler)
+	}
+	account.Use(auth())
+
 	route.Run(":8080")
 }
 
-func loginHandler(c *gin.Context) {
+func auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		user := session.Get("user")
+		if user == nil {
+			c.String(http.StatusNotAcceptable, "You should not pass!")
+			log.Println("A strangers attempted to log in!")
+		} else {
+			c.Next()
+		}
+	}
+}
 
+func loginHandler(c *gin.Context) {
+	session := sessions.Default(c)
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	if strings.Trim(username, " ") == "" || strings.Trim(password, " ") == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Parameters can't be empty"})
+		return
+	}
+	if username == "hello" && password == "itsme" {
+		session.Set("user", username)
+		err := session.Save()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate session token"})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"message": "Successfully authenticated user"})
+		}
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
+	}
 }
 
 func logoutHandler(c *gin.Context) {
-
+	session := sessions.Default(c)
+	user := session.Get("user")
+	if user == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid session token"})
+	} else {
+		log.Println(user)
+		session.Delete("user")
+		session.Save()
+		c.JSON(http.StatusOK, gin.H{"message": "Successfully logged out"})
+	}
 }
 
 func registerHandler(c *gin.Context) {
 
+}
+
+func profileHandler(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get("user")
+	c.JSON(http.StatusOK, user)
 }
