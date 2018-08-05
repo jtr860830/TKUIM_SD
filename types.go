52d4d1d64caa2ed784a42bf9563a7b041415fb84
Jezrien Hsieh <jtr860830@yahoo.com.tw>
2018-08-06 03:46:02 +0800

    Add data model types

---
+++ types.go
@@ -0,0 +1,57 @@
+package main // import "github.com/jtr860830/SD-Backend"
+
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+// User data model
+type User struct {
+	gorm.Model
+	Username string `gorm:"unique;not null"`
+	Password string `gorm:"not null"`
+	Email    string
+	Birthday time.Time `gorm:"not null"`
+	Sticker  string
+	Friend   []*User  `gorm:"many2many:friendships;association_jointable_foreignkey:friend_id"`
+	Groups   []*Group `gorm:"many2many:user_group;"`
+	Schedule []userSchedule
+	Backup   []backup
+}
+
+// Group data model
+type Group struct {
+	gorm.Model
+	Name     string
+	Color    string
+	Sticker  string
+	Users    []*User `gorm:"many2many:user_group;"`
+	Schedule []groupSchedule
+}
+
+type userSchedule struct {
+	gorm.Model
+	UserID   uint
+	Event    string
+	Time     time.Time
+	Location string
+	Color    string
+	Note     string
+}
+
+type groupSchedule struct {
+	gorm.Model
+	GroupID   uint
+	SponsorID uint
+	Event     string
+	Time      time.Time
+	Location  string
+	Color     string
+	Note      string
+}
+
+type backup struct {
+	gorm.Model
+	UserID uint
+	Info   string
+}
